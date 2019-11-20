b6c055522a95abd75f2ef2ed8fde73239a0d4b66
Bola Ahmed Buari <bola_ahmed2002@yahoo.com>
2019-11-20 10:52:21 +0100

    Connect actions to components

--- src/components/BooksForm.jsx
+++ src/components/BooksForm.jsx
@@ -1,9 +1,13 @@
 import React from 'react';
+import { connect } from 'react-redux';
+import PropTypes from 'prop-types';
 import styles from '../css/modules/BooksForm.module.css';
+import { createBook } from '../actions';
+import formIsValid from '../helpers/validateForm';
 
 const categories = ['Action', 'Biography', 'History', 'Horror', 'Kids', 'Learning', 'Sci-Fi'];
 
-const SelectCategories = () => {
+const SelectCategories = ({ value, handleChange }) => {
   const options = categories.map(category => (
     <option key={category} value={category}>
       {category}
@@ -11,7 +15,7 @@ const SelectCategories = () => {
   ));
 
   return (
-    <select name="categories" id="category">
+    <select value={value} onChange={handleChange} name="category" id="book-category" required>
       <option key="placeholder" value="">
         Category
       </option>
@@ -20,31 +24,99 @@ const SelectCategories = () => {
   );
 };
 
-const BooksForm = () => (
-  <form action="#" className={`${styles.form} d-flex col`}>
-    <h2 className={styles.form_title}>Add new book</h2>
-    <div className="d-flex jc-sb">
-      <div className={styles.title}>
-        <input
-          className="data"
-          placeholder="Book Title"
-          type="text"
-          name="title"
-          id="book-title"
-          minLength="1"
-          required
-        />
-      </div>
-      <div className={styles.select}>
-        <SelectCategories />
-      </div>
-      <div className={styles.button}>
-        <button type="button" id="add-book">
-          Add Book
-        </button>
-      </div>
-    </div>
-  </form>
-);
+class BooksForm extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      book: {
+        title: '',
+        category: '',
+      },
+    };
 
-export default BooksForm;
+    this.handleChange = this.handleChange.bind(this);
+    this.handleSubmit = this.handleSubmit.bind(this);
+  }
+
+  handleChange({ target: { value, name } }) {
+    const { book } = this.state;
+    this.setState({
+      book: {
+        ...book,
+        [name]: value,
+      },
+    });
+    formIsValid();
+  }
+
+  handleSubmit(event) {
+    const {
+      state: { book },
+      props: { createBook },
+    } = this;
+    event.preventDefault();
+    if (formIsValid()) {
+      createBook(book);
+      this.setState({
+        book: {
+          title: '',
+          category: '',
+        },
+      });
+    }
+  }
+
+  render() {
+    const {
+      book: { title, category },
+    } = this.state;
+    return (
+      <form action="#" className={`${styles.form} d-flex col`}>
+        <h2 className={styles.form_title}>Add new book</h2>
+        <div className="d-flex col as-c error-message" id="error-message">
+          <div className="title-message">
+            Please enter a valid book name (Minimum of one character)
+          </div>
+          <div className="category-message">Please choose one book category</div>
+        </div>
+        <div className="d-flex jc-sb">
+          <div className={styles.title}>
+            <input
+              className="data"
+              placeholder="Book Title"
+              type="text"
+              name="title"
+              value={title}
+              id="book-title"
+              minLength="1"
+              onChange={this.handleChange}
+              required
+            />
+          </div>
+          <div className={styles.select}>
+            <SelectCategories handleChange={this.handleChange} value={category} />
+          </div>
+          <div className={styles.button}>
+            <button onClick={this.handleSubmit} type="button" id="add-book">
+              Add Book
+            </button>
+          </div>
+        </div>
+      </form>
+    );
+  }
+}
+
+SelectCategories.propTypes = {
+  value: PropTypes.string.isRequired,
+  handleChange: PropTypes.func.isRequired,
+};
+
+BooksForm.propTypes = {
+  createBook: PropTypes.func.isRequired,
+};
+
+export default connect(
+  ({ book }) => ({ book }),
+  { createBook },
+)(BooksForm);
